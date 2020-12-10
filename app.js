1bbd5e5d8cae9f36cc7e80956c2de5f908daf267
amirahad <arahad4783@gmail.com>
2020-12-10 13:27:13 +0600

    added button to change background color randomly

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const daysEL = document.getElementById('days');
 const hoursEL = document.getElementById('hours');
 const minutesEL = document.getElementById('minutes');
 const secondsEL = document.getElementById('seconds');
-
+const button = document.querySelector('#btn').addEventListener('click', changeHex);
+const body = document.querySelector('body');
+const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const value = document.querySelector('#hex-value');
+ 
 const newYears = "1 jan 2021";
 
 function countdown(){
@@ -23,6 +27,16 @@ function countdown(){
 }
 function formatTime(time){
     return time < 10 ? `0${time}` : time;
+}
+
+function changeHex(){
+    let hex = '#'
+    for(let i = 0; i < 6; i++){
+        const index = Math.floor(Math.random()*hexValues.length)
+        hex += hexValues[index]
+    }
+    value.textContent = hex;
+    body.style.backgroundColor = hex;
 }
 
 countdown();
